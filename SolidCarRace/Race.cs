027441375e2ae35170c9858b852a173918371e78
bgabor88 <coindealerpro@gmail.com>
2022-02-07 13:44:10 +0100

    Copy solution from my preivous attempt before forking.

--- SolidCarRace/Race.cs
+++ SolidCarRace/Race.cs
@@ -1,41 +1,26 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace SolidCarRace
 {
     public class Race
     {
-        private readonly int numberOfLaps;
+        private readonly int _numberOfLaps;
 #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
-        private readonly List<Car>? cars;
+        private readonly List<Car>? _cars;
 #pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
 
-        public Race(int laps, List<Car>? cars)
+        public Race(int laps, List<Car> cars)
         {
-            this.numberOfLaps = laps;
-            this.cars = cars;
+            _numberOfLaps = laps;
+            _cars = cars;
         }
 
         public int CalculateFuel()
         {
-            var fuel = 0;
-
-            if (numberOfLaps <= 0 || cars is null)
-                return 0;
-
-            foreach (var car in cars)
-            {
-                if (car is Ford)
-                {
-                    fuel += 4;
-                }
-                if (car is Ferrari)
-                {
-                    fuel += 2;
-                }
-            }
-
-            
-            return fuel * numberOfLaps;
+            var laps = _numberOfLaps <= 0 ? 0 : _numberOfLaps;
+            var fuel = _cars?.Sum(car => car.GetFuelConsumption());
+            return fuel * laps ?? 0;
         }
     }
 }
